19e1048539f70020945bbb2f79428347be0e0a33
eliashead <eliashead.costa@gmail.com>
2023-02-08 08:45:36 -0300

    feat: list competition

--- src/components/ListCompetition/index.tsx
+++ src/components/ListCompetition/index.tsx
@@ -3,26 +3,45 @@ import React from 'react'
 
 export default function ListCompetition({ competitions }: PropsCompetition) {
   return (
-    <div>
-      <>
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {competitions.map((competition) => {
-              return (
-                <tr key={competition.competition_id}>
-                  <td>{competition.competition_id} - </td>
-                  <td>{competition.competition_name}</td>
+    <div className="flex flex-col">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="overflow-hidden">
+            <table className="min-w-full">
+              <thead className="border-b">
+                <tr>
+                  <th
+                    scope="col"
+                    className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    #
+                  </th>
+                  <th
+                    scope="col"
+                    className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    Competições
+                  </th>
                 </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </>
+              </thead>
+              <tbody>
+                {competitions.map((competition) => {
+                  return (
+                    <tr className="border-b" key={competition.competition_id}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {competition.competition_id}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {competition.competition_name}
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
