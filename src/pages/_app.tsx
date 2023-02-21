a87000e27eea46f7a35124b7b241750955e6ed36
eliashead <eliashead.costa@gmail.com>
2023-02-21 20:20:27 -0300

    feat: add nprogress load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,26 @@
 import type { AppProps } from 'next/app'
 
+// # Faça a importação do next/router e do NProgress
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+// # Import também o next/head, você vai precisar dele
+import Head from 'next/head'
+
 import '../styles/globals.css'
 
+// # Esses são os eventos disparados quando o usuário muda de página
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/npprogress.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
